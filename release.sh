e6fec1b62d519b6580b2145a42ae5f8895206dd5
alfred-brown <alfred.brown@oddball.io>
2022-08-18 15:51:18 -0400

    Adding release.sh

---
+++ release.sh
@@ -0,0 +1,17 @@
+#!/bin/env bash
+
+NOW=$(date '+%Y%m%d%H%M%S')
+
+if (
+  set -x -o nounset
+  aws ecr get-login-password --region "${AWS_DEFAULT_REGION}" | \
+  docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}".dkr.ecr."${AWS_DEFAULT_REGION}".amazon.com
+); then
+
+sha=$(git rev-parse HEAD)
+#repository="atlantis-repo"
+tag="${sha}-${NOW}"
+
+echo "ATLANTIS_IMAGE_TAG=${tag}" >> "${GITHUB_ENV}"
+
+fi
