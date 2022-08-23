f90a2aae6ed4bb68c7eab40bfc2425b694de4a8e
Aboogie <albrown.ctr@kr.af.mil>
2022-08-23 14:40:45 +0300

    updating pre-commit hooks

--- release.sh
+++ release.sh
@@ -3,16 +3,16 @@
 
 NOW=$(date '+%Y%m%d%H%M%S')
 
-ID=608911059386
+ID=1234564325
 
-ID2=123456432
+ID2=608911059386
 echo "${ID} + ${ID2}"
 
 
 
 if (
   set -x -o nounset
-  aws ecr get-login-password --region "${AWS_DEFAULT_REGION}" | docker login --username AWS --password-stdin "${ID}".dkr.ecr."${AWS_DEFAULT_REGION}".amazon.com
+  aws ecr get-login-password --region "${AWS_DEFAULT_REGION}" | docker login --username AWS --password-stdin "${ID2}".dkr.ecr."${AWS_DEFAULT_REGION}".amazon.com
 ); then
 
 sha=$(git rev-parse HEAD)
